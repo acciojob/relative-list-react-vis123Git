22953de3cf2dfb6d9414c9a69ca81e89dc5e735f
VishalKumarSain <vishaldhoria007@gmail.com>
2024-09-19 08:43:11 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
 
+const data = ["relativeListItem1", "relativeListItem2", "relativeListItem3", "relativeListItem4", "relativeListItem5"];
 const App = () => {
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+      <ol id="list">
+        {data.map((item, index) => (
+          <li key={item} id={item}>{item}</li>
+        ))}
+      </ol>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
